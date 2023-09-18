48dd9aaf2083532fb3a4c6815434981293bcde8d
Gaston Mosteiro <gastonmosteiro@hotmail.com>
2023-09-18 19:22:46 -0300

    ;

--- src/main/java/GimController/GetImage.java
+++ src/main/java/GimController/GetImage.java
@@ -17,23 +17,12 @@ public class GetImage extends HttpServlet {
             throws ServletException, IOException {
         // Configura el tipo de contenido para una imagen JPEG
         response.setContentType("image/jpeg");
-                System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaa")
-
-        // Obtiene la ruta real del archivo de imagen
         String path = getServletContext().getRealPath(File.separator);
-        
+
         File file = new File(path + "imagenes/img1.jpg");
-        
-        // Lee la imagen desde el archivo
         BufferedImage imagen = ImageIO.read(file);
-        
-        // Obtiene el flujo de salida de la respuesta
         OutputStream respuesta = response.getOutputStream();
-        
-        // Escribe la imagen en el flujo de respuesta en formato JPEG
         ImageIO.write(imagen, "jpeg", respuesta);
-        
-        // Cierra el flujo de respuesta
         respuesta.close();
     }
 
