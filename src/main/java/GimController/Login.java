216f11e82cd8e9307c5ce5c2a7a4db78cc0927e5
xSanPrax <92343442+xSanPrax@users.noreply.github.com>
2023-09-20 10:36:00 -0300

    poxes varios

--- src/main/java/GimController/Login.java
+++ src/main/java/GimController/Login.java
@@ -12,25 +12,30 @@ import logic.Usuario.Usuario;
 import logic.Usuario.controllers.ControllerInicioSesion;
 import logic.Usuario.controllers.IControllerInicioSesion;
 
-// @WebServlet("/Login")
+//@WebServlet("/Login")
 public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        Fabrica factory = new Fabrica();
-        IControllerInicioSesion controller = factory.getControllerInicioSesion();
+        try {
+            Fabrica factory = new Fabrica();
+            IControllerInicioSesion controller = factory.getControllerInicioSesion();
 
-        System.out.println("Data" + email + " | " + password);
+            System.out.println("Data: " + email + " | " + password);
 
-        Usuario usuarioLogeado = controller.iniciarSesion(email, password);
-        System.out.println("Usuario: " + usuarioLogeado.getNickname());
-        if (usuarioLogeado != null) {
-            response.sendRedirect("menuPrincipal.jsp");
-        } else {
-            response.sendRedirect("login.jsp?error=1");
+            Usuario usuarioLogeado = controller.iniciarSesion(email, password);
+
+            if (usuarioLogeado != null) {
+                System.out.println("Usuario: " + usuarioLogeado.getNickname());
+                response.sendRedirect("menuPrincipal.jsp");
+            } else {
+                response.sendRedirect("login.jsp?error=1");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendRedirect("login.jsp?error=2"); // Redirige con un código de error apropiado
         }
     }
-
 }
