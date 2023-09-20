73bba5d38a9d80fd8e343a0e36320d9dae62076c
xSanPrax <92343442+xSanPrax@users.noreply.github.com>
2023-09-20 19:09:02 -0300

    hice andar el login skere

--- src/main/java/GimController/Login.java
+++ src/main/java/GimController/Login.java
@@ -1,7 +1,6 @@
 package GimController;
 
 import java.io.IOException;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,6 +11,9 @@ import logic.Usuario.Usuario;
 import logic.Usuario.controllers.ControllerInicioSesion;
 import logic.Usuario.controllers.IControllerInicioSesion;
 
+/**
+ * Servlet implementation class Login
+ */
 //@WebServlet("/Login")
 public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -20,22 +22,30 @@ public class Login extends HttpServlet {
         String password = request.getParameter("password");
 
         try {
+            // Crear una instancia de la fábrica
             Fabrica factory = new Fabrica();
+            
+            // Obtener el controlador de inicio de sesión
             IControllerInicioSesion controller = factory.getControllerInicioSesion();
 
+            // Imprimir los datos (esto puede ser útil para propósitos de depuración)
             System.out.println("Data: " + email + " | " + password);
 
+            // Iniciar sesión
             Usuario usuarioLogeado = controller.iniciarSesion(email, password);
 
             if (usuarioLogeado != null) {
+                // Si el inicio de sesión es exitoso, redirigir a la página principal
                 System.out.println("Usuario: " + usuarioLogeado.getNickname());
                 response.sendRedirect("menuPrincipal.jsp");
             } else {
+                // Si el inicio de sesión falla, redirigir de nuevo a la página de inicio de sesión con un mensaje de error
                 response.sendRedirect("login.jsp?error=1");
             }
         } catch (Exception e) {
-            e.printStackTrace();
-            response.sendRedirect("login.jsp?error=2"); // Redirige con un código de error apropiado
+            // Manejar la excepción adecuadamente (log, redirigir, etc.)
+            e.printStackTrace(); // Considera usar un sistema de registro como Log4j
+            response.sendRedirect("login.jsp?error=2"); 
         }
     }
 }
