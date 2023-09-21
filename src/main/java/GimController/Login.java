ffd1436e2e613af46528664a00e23b6b5f9e7ff2
GastonMosteiro <gastonmosteiro@hotmail.com>
2023-09-21 09:51:40 -0300

    Commit

--- src/main/java/GimController/Login.java
+++ src/main/java/GimController/Login.java
@@ -7,45 +7,34 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import logic.Fabrica;
-import logic.Usuario.Usuario;
+import logic.Usuario.Sesion;
 import logic.Usuario.controllers.ControllerInicioSesion;
 import logic.Usuario.controllers.IControllerInicioSesion;
 
-/**
- * Servlet implementation class Login
- */
-//@WebServlet("/Login")
 public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-
         try {
-            // Crear una instancia de la fábrica
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+
             Fabrica factory = new Fabrica();
-            
-            // Obtener el controlador de inicio de sesión
             IControllerInicioSesion controller = factory.getControllerInicioSesion();
 
-            // Imprimir los datos (esto puede ser útil para propósitos de depuración)
-            System.out.println("Data: " + email + " | " + password);
-
-            // Iniciar sesión
-            Usuario usuarioLogeado = controller.iniciarSesion(email, password);
+            Sesion usuarioLogeado = controller.iniciarSesion(email, password);
 
             if (usuarioLogeado != null) {
-                // Si el inicio de sesión es exitoso, redirigir a la página principal
-                System.out.println("Usuario: " + usuarioLogeado.getNickname());
+                // Almacena el usuario en la sesión
+                request.getSession().setAttribute("usuarioLogeado", usuarioLogeado);
+
                 response.sendRedirect("menuPrincipal.jsp");
             } else {
-                // Si el inicio de sesión falla, redirigir de nuevo a la página de inicio de sesión con un mensaje de error
                 response.sendRedirect("login.jsp?error=1");
             }
         } catch (Exception e) {
-            // Manejar la excepción adecuadamente (log, redirigir, etc.)
-            e.printStackTrace(); // Considera usar un sistema de registro como Log4j
-            response.sendRedirect("login.jsp?error=2"); 
+            System.out.println("Error validando el login " + e);
+            e.printStackTrace();
+            response.sendRedirect("login.jsp?error=2");
         }
     }
 }
--- src/main/webapp/header.jsp
+++ src/main/webapp/header.jsp
@@ -1,18 +1,38 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+	crossorigin="anonymous"
+/>
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand"  href="menuPrincipal.jsp">GymPap</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-      </div>
-    </div>
-  </div>
+	<div class="container-fluid">
+		<a class="navbar-brand" style="margin-right: 25%; margin-left: 10%" href="menuPrincipal.jsp">GymPap</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNavAltMarkup"
+			aria-controls="navbarNavAltMarkup"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-link active" aria-current="page" href="#">Inicio</a>
+				<a class="nav-link active" aria-current="page" href="#">Clases</a>
+				<a class="nav-link" href="#">Actividades Deportivas</a>
+				<a class="nav-link" href="#">Usuario</a>
+			</div>
+		</div>
+		<div class="navbar-nav">
+			<% logic.Usuario.Sesion usuarioLogeado = (logic.Usuario.Sesion)
+			request.getSession().getAttribute("usuarioLogeado"); if (usuarioLogeado != null) { %>
+			<img style="width: 40px; margin-right: 5px" src="<%= usuarioLogeado.getProfileImage() %>" />
+			<a class="nav-link"><%= usuarioLogeado.getNombre() + " " + usuarioLogeado.getApellido() %></a>
+			<% } %>
+		</div>
+	</div>
 </nav>
