671a44a9e71e6ce67cb67e98506f2557e8296bce
GastonMosteiro <gastonmosteiro@hotmail.com>
2023-10-19 09:50:33 -0300

    Ready el servlet

--- src/main/java/GimController/RegistroDictadoClase.java
+++ src/main/java/GimController/RegistroDictadoClase.java
@@ -50,20 +50,28 @@ public class RegistroDictadoClase extends HttpServlet {
             response.setCharacterEncoding("UTF-8");
             JSONObject jsonResponse = new JSONObject();
 
-            boolean validate = controller.validateDataWeb(clase, fechaStr);
+            boolean validate = controller.validateDataWeb(socio, clase);
+            System.out.println("validate: " + validate);
 
             if (!validate) {
 
-                jsonResponse.put("ERROR", validate);
+                jsonResponse.put("ERROR", !validate);
                 jsonResponse.put("Message", " Ya existe un registro para los datos ingresados ");
+                response.getWriter().write(jsonResponse.toString());
+
             } else {
+
                 // String nicknameSocio, String nombreClase, LocalDate fechaReg
                 boolean registro = controller.addRegistroDictadoWeb(socio, clase, fecha);
+                System.out.println("registro: " + registro);
 
-                jsonResponse.put("ERROR", registro);
-                jsonResponse.put("Message", !registro ? "Error al crear registro " : "Eliminado con exito ");
+                jsonResponse.put("ERROR", !registro);
+                jsonResponse.put("Message", !registro ? "Error al crear registro " : "Creado con exito ");
+
+                System.out.println("response: " + jsonResponse.toString());
+
+                response.getWriter().write(jsonResponse.toString());
             }
-            response.getWriter().write(jsonResponse.toString());
 
         } catch (Exception e) {
             // Manejar el error apropiadamente y enviar una respuesta de error al cliente si
