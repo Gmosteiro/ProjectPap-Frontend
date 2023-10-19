0bcab0373ec0130fb1feea83949cad09fb130589
GastonMosteiro <gastonmosteiro@hotmail.com>
2023-10-18 21:34:34 -0300

    Commit

---
+++ src/main/java/GimController/RegistroDictadoClase.java
@@ -0,0 +1,77 @@
+package GimController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.json.JSONObject;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import logic.Fabrica;
+import logic.Usuario.ManejadorUsuarios;
+import logic.Usuario.Registro;
+import logic.Usuario.controllers.IControllerRegistroDictado;
+
+@WebServlet("/RegistroDictadoClase")
+
+public class RegistroDictadoClase extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+
+            System.out.println("En Registro Dictado Clase");
+            BufferedReader reader = request.getReader();
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            reader.close();
+
+            JSONObject jsonData = new JSONObject(sb.toString());
+
+            String socio = jsonData.getString("socio");
+            String clase = jsonData.getString("clase");
+            String fechaStr = jsonData.getString("fecha");
+
+            LocalDate fecha = LocalDate.parse(fechaStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+            Fabrica factory = new Fabrica();
+
+            IControllerRegistroDictado controller = factory.getControllerRegistroDictado();
+
+            // response
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            JSONObject jsonResponse = new JSONObject();
+
+            boolean validate = controller.validateDataWeb(clase, fechaStr);
+
+            if (!validate) {
+
+                jsonResponse.put("ERROR", validate);
+                jsonResponse.put("Message", " Ya existe un registro para los datos ingresados ");
+            } else {
+                // String nicknameSocio, String nombreClase, LocalDate fechaReg
+                boolean registro = controller.addRegistroDictadoWeb(socio, clase, fecha);
+
+                jsonResponse.put("ERROR", registro);
+                jsonResponse.put("Message", !registro ? "Error al crear registro " : "Eliminado con exito ");
+            }
+            response.getWriter().write(jsonResponse.toString());
+
+        } catch (Exception e) {
+            // Manejar el error apropiadamente y enviar una respuesta de error al cliente si
+            // es necesario
+            System.out.println("Catch " + e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("{Error : " + e.getMessage() + "}");
+        }
+    }
+
+}
