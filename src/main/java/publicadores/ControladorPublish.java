04f62f5147f7d09517f58d802e68e3fa48dea6a8
xSanPrax <92343442+xSanPrax@users.noreply.github.com>
2023-10-25 19:23:22 -0300

    demasiado toquete en el pom para que funquen lso primeros 30 min del tuto del profe

---
+++ src/main/java/publicadores/ControladorPublish.java
@@ -0,0 +1,13 @@
+/**
+ * ControladorPublish.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package publicadores;
+
+public interface ControladorPublish extends java.rmi.Remote {
+    public void obtenerActividadPorNombre(java.lang.String arg0) throws java.rmi.RemoteException;
+    public void iniciarSesion(java.lang.String arg0, java.lang.String arg1) throws java.rmi.RemoteException;
+}
