b6b9542dba1423f9e639427409c9e3e9e00cc471
xSanPrax <92343442+xSanPrax@users.noreply.github.com>
2023-11-14 14:26:57 -0300

    esto anda tonga

--- src/main/java/GimController/GetClases.java
+++ src/main/java/GimController/GetClases.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import javax.xml.rpc.ServiceException;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -21,101 +20,82 @@ import publicadores.DtClase;
 @WebServlet("/getClases")
 public class GetClases extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String nickname = request.getParameter("nickname");
-		String nombreClase = request.getParameter("nombreClase");
-		String nombreActividad = request.getParameter("nombreActividad");
-		String tablaConAccion = request.getParameter("tablaconaccion");
-		// Si necesitan agregar otro parametro lo meten aca
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String nickname = request.getParameter("nickname");
+        String nombreClase = request.getParameter("nombreClase");
+        String nombreActividad = request.getParameter("nombreActividad");
+        String tablaConAccion = request.getParameter("tablaconaccion");
 
-		ControladorPublishServiceLocator cps = new ControladorPublishServiceLocator();
-		ControladorPublish port = null;
-		try {
-			port = cps.getControladorPublishPort();
-		} catch (ServiceException e) {
+        ControladorPublishServiceLocator cps = new ControladorPublishServiceLocator();
+        ControladorPublish port = null;
+        try {
+            port = cps.getControladorPublishPort();
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
 
-			e.printStackTrace();
-		}
+        DtClase[] clases = null;
 
-		DtClase[] clases = null;
-		// y crean otra condicion aca (lo mejor seria pasarlo a un switch)
+        if (nickname != null && nickname.length() > 0) {
+            clases = port.getClasesByUser(nickname);
+        } else if (nombreClase != null && nombreClase.length() > 0) {
+            DtClase nuevaClase = port.obtenerClasePorNombre(nombreClase);
+            clases = new DtClase[]{nuevaClase};
+        } else if (nombreActividad != null && nombreActividad.length() > 0) {
+            DtActividadDeportiva actividadBuscada = port.obtenerActividadPorNombre(nombreActividad);
+            clases = port.obtenerClasesPorActividad(actividadBuscada);
+        }
 
-		if (nickname != null && nickname.length() > 0) {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
-			clases = port.getClasesByUser(nickname);
+        out.println("<table class='table table-bordered'>");
+        out.println("<thead>");
+        out.println("<tr>");
+        out.println("<th>Nombre</th>");
+        out.println("<th>Fecha</th>");
+        out.println("<th>Fecha de Registro</th>");
+        out.println("<th>Hora</th>");
+        out.println("<th>URL</th>");
+        out.println("<th>Imagen</th>");
 
-		} else if (nombreClase != null && nombreClase.length() > 0) {
+        if (tablaConAccion != null && tablaConAccion.length() > 0) {
+            out.println("<th>Accion</th>");
+        }
 
-			DtClase nuevaClase = port.obtenerClasePorNombre(nombreClase);
+        out.println("</tr>");
+        out.println("</thead>");
+        out.println("<tbody>");
 
-			clases[1] = nuevaClase;
+        if (clases != null) { // Verifica si la lista de clases no es null
+            if (clases.length > 0) {
+                for (DtClase clase : clases) {
+                    out.println("<tr>");
+                    out.println("<td>" + clase.getNombre() + "</td>");
+                    out.println("<td>" + clase.getFecha() + "</td>");
+                    out.println("<td>" + clase.getFechaRegistro() + "</td>");
+                    out.println("<td>" + clase.getHora() + "</td>");
+                    out.println("<td>" + clase.getUrl() + "</td>");
+                    out.println("<td> <img src=\"data:image/png;base64," + clase.getImagen()
+                            + " alt=\"Imagen\" style=\"width: 40px; height: 40px; border-radius: 50%\" /> </td>");
 
-		} else if (nombreActividad != null && nombreActividad.length() > 0) {
+                    if (tablaConAccion != null && tablaConAccion.length() > 0) {
+                        out.println(
+                                "<td><button class=\"btn btn-info btn-block btn-round\" data-action=\"registrar\" type=\"button\" onclick=\"enviarInfo('"
+                                        + clase.getNombre() + "');\">Registrar</button></td>");
+                    }
 
-			DtActividadDeportiva actividadBuscada = port.obtenerActividadPorNombre(nombreActividad);
+                    out.println("</tr>");
+                }
+            } else {
+                out.println("<tr><td colspan='5'>No se encontraron clases</td></tr>");
+            }
+        } else {
+            out.println("<tr><td colspan='5'>Error: La lista de clases es nula</td></tr>");
+        }
 
-			clases = port.obtenerClasesPorActividad(actividadBuscada);
-
-		}
-
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-
-		out.println("<table class='table table-bordered'>");
-		out.println("<thead>");
-		out.println("<tr>");
-		out.println("<th>Nombre</th>");
-		out.println("<th>Fecha</th>");
-		out.println("<th>Fecha de Registro</th>");
-		out.println("<th>Hora</th>");
-		out.println("<th>URL</th>");
-		out.println("<th>Imagen</th>");
-
-		if (tablaConAccion != null && tablaConAccion.length() > 0) {
-
-			out.println("<th>Accion</th>");
-		}
-
-		out.println("</tr>");
-		out.println("</thead>");
-		out.println("<tbody>");
-
-		if (clases != null && clases.length == 0) {
-
-			for (DtClase clase : clases) {
-				out.println("<tr>");
-				out.println("<td>" + clase.getNombre() + "</td>");
-				out.println("<td>" + clase.getFecha() + "</td>");
-				out.println("<td>" + clase.getFechaRegistro() + "</td>");
-				out.println("<td>" + clase.getHora() + "</td>");
-				out.println("<td>" + clase.getUrl() + "</td>");
-
-				out.println("<td> <img src=\"data:image/png;base64," + clase.getImagen()
-						+ " alt=\"Imagen\" style=\"width: 40px; height: 40px; border-radius: 50%\" /> </td>");
-				if (tablaConAccion != null && tablaConAccion.length() > 0) {
-
-					out.println(
-							"<td><button class=\"btn btn-info btn-block btn-round\" data-action=\"registrar\" type=\"button\" onclick=\"enviarInfo('"
-									+ clase.getNombre() + "');\">Registrar</button></td>");
-				}
-
-				out.println("</tr>");
-			}
-		} else {
-			out.println("<tr><td colspan='5'>No se encontraron clases</td></tr>");
-		}
-
-		out.println("</tbody>");
-		out.println("</table>");
-	}
-
-	private String formatFecha(LocalDate fecha) {
-
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-
-		return fecha.atStartOfDay().format(formatter);
-
-	}
-
+        out.println("</tbody>");
+        out.println("</table>");
+    }
 }
